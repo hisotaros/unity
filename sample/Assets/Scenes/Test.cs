16d05e85be64dd5e0e1758e092b5e569f4f6c7ab
hisotaros <hisota.mmm@gmail.com>
2019-01-27 11:07:53 +0900

    演算子の練習 通常計算

--- sample/Assets/Scenes/Test.cs
+++ sample/Assets/Scenes/Test.cs
@@ -7,9 +7,24 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string name;
-        name = "kitamura";
-        Debug.Log(name);
+        int answer;
+        answer = 1 + 2;
+        Debug.Log(answer);
+
+        answer = 3 - 4;
+        Debug.Log(answer);
+
+        answer = 5 * 6;
+        Debug.Log(answer);
+
+        answer = 8 / 4;
+        Debug.Log(answer);
+
+        int n1 = 8;
+        int n2 = 9;
+        answer = n1 + n2;
+        Debug.Log(answer);
+
     }
 
 }
